fa2e5d699c9d05828e5b7aacc61757425a4f0467
jorros <joergrosenauer@gmail.com>
2016-03-13 15:07:25 +0000

    Added Parser module

---
+++ Parser/src/main/kotlin/de/jorros/league/parser/Parser.kt
@@ -0,0 +1,11 @@
+package de.jorros.league.parser
+
+
+class Parser {
+
+}
+
+fun main(args: Array<String>) {
+    // if you want to run the code without the page, select
+    System.out.println("Hello world")
+}
